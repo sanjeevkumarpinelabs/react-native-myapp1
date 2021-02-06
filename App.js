69b6a9c53458f1490a13b279ed931ee0db625248
Sanjeev Kumar <sanjeev.kumar@pinelabs.com>
2021-02-06 11:29:30 +0530

    Added 2 text fields , setState react hook has been used , two way binding done using setState for setting the state and simple state values for showing in text display and alert

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useState} from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import {TextInput} from 'react-native-gesture-handler'
 
 export default function App() {
+  const [state,setState] = useState({name:"Sanjeev" , age:"40"});
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is ! </Text>
+      <TextInput onChangeText={text=> setState({...state,name:text})} value={state.name} style={{borderColor:'red' , width:300, borderWidth: 2}}></TextInput>
+      <TextInput onChangeText={text=> setState({...state,age:text})} value={state.age} style={{borderColor:'green' , width:300, borderWidth: 2}}></TextInput>
+      <Button title = "Save 10" onPress={()=>{alert(`${state.name} is of age ${state.age}` ); 
+      console.log("this is the console log")}}></Button>
       <StatusBar style="auto" />
     </View>
   );
